01f63b11105c213d3a4c030478879133faf5eeae
Your Name <piousvir@icloud.com>
2024-11-12 22:49:24 -0600

    got the login/logout working

--- app/layout.tsx
+++ app/layout.tsx
@@ -51,7 +51,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <body>
         <Providers>{children}</Providers>
       </body>
