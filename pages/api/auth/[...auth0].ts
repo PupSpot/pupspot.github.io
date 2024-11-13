963addc01393e7c9e654bec0d0698ea7bb4e9ac1
Your Name <piousvir@icloud.com>
2024-11-13 17:51:08 -0600

    i think i fixed it maybe

--- pages/api/auth/[...auth0].ts
+++ pages/api/auth/[...auth0].ts
@@ -1,11 +1,19 @@
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
+import { NextApiRequest, NextApiResponse } from 'next';
 
 export default handleAuth({
-  login: handleLogin({
-    returnTo: '/',
-    authorizationParams: {
-      prompt: 'login',
+  login: async (req: NextApiRequest, res: NextApiResponse) => {
+    try {
+      await handleLogin(req, res, {
+        returnTo: '/',
+        authorizationParams: {
+          prompt: 'login',
+        }
+      });
+    } catch (error) {
+      console.error(error);
+      res.status(500).end();
     }
-  })
+  }
 });
 
